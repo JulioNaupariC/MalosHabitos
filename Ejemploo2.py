7a91d5e2418f1031c7c48fb5d09df50cbefaf773
Ejerciocio 1 <71011535@continental.edu.pe>
2024-09-09 15:22:58 -0500

    Nodificacion ejerciicio 1

--- Ejemploo2.py
+++ Ejemploo2.py
@@ -1,12 +1,15 @@
-def calcular(a, b, c):
-    res = a * b + c
-    return res
+def multiplicacion(factor1, factor2):
+    producto = factor1 * factor2
+    return producto
 
-def principal():
-    x = 5
-    y = 3
-    z = 7
-    resultado = calcular(x, y, z)
-    print("El resultado es:", resultado)
 
-principal()
+if __name__ == "__main__":
+    # Corrige 'imput' a 'input' y la indentación
+    multiplicando = float(input("Ingrese el primer número: "))
+    multiplicador = float(input("Ingrese el segundo número: "))
+
+    # Llama a la función de multiplicación y almacena el resultado
+    resultado = multiplicacion(multiplicando, multiplicador)
+
+    # Imprime el resultado de manera formateada
+    print(f"{multiplicando} * {multiplicador} = {resultado}")
