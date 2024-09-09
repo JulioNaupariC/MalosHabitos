6795dcb586eeb911c4a9f7235be7274840075b39
Ejerciocio 1 <71011535@continental.edu.pe>
2024-09-09 15:04:34 -0500

    Nodificacion ejerciicio 1

---
+++ Ejemploo1.py
@@ -0,0 +1,15 @@
+
+def multiplicacion(factor1, factor2):
+    producto = factor1 * factor2
+    return producto
+
+if __name__ == "__main__":
+    # Corrige 'imput' a 'input' y la indentación
+    multiplicando = float(input("Ingrese el primer número: "))
+    multiplicador = float(input("Ingrese el segundo número: "))
+
+    # Llama a la función de multiplicación y almacena el resultado
+    resultado = multiplicacion(multiplicando, multiplicador)
+
+    # Imprime el resultado de manera formateada
+    print(f"{multiplicando} * {multiplicador} = {resultado}")
